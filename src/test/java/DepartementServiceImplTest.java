5adcd010879dc5e0af503dc3d6b62d0e12ebdb1c
Dhouha <92980075+dhouhe@users.noreply.github.com>
2023-11-16 18:28:04 +0100

    Update DepartementServiceImplTest.java

--- src/test/java/DepartementServiceImplTest.java
+++ src/test/java/DepartementServiceImplTest.java
@@ -2,6 +2,8 @@ import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
